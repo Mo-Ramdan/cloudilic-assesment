57a6ffd720c6d468642bc02c534e1488f4425e4c
Mohamed Ramadan <mohameda7maa@gmail.com>
2024-09-06 23:00:14 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, HTTPException
+from utils import scrape_then_summarize
+from models import UrlRequest
+from fastapi.middleware.cors import CORSMiddleware
+import logging
+import os 
+
+app = FastAPI()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[os.environ.get("FRONTEND_URL")],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"], 
+)
+
+@app.post("/scrape-summary")
+async def scrape_and_summarize(request: UrlRequest):
+    url = request.url
+    try:
+        logger.info(f"Scraping and summarizing: {url}")
+        summary = scrape_then_summarize(url)
+        if not summary:
+            logger.warning(f"No Summary return for URL: {url}")
+            raise HTTPException(
+                status_code=404, detail="Summary not found or scraping failed"
+            )
+    except Exception as e:
+        logger.error(f"Error occurred during scraping: {e}")
+        raise HTTPException(status_code=400, detail="Can't scrape or summarize")
+    return {"data": summary}
---
+++ utils.py
@@ -0,0 +1,66 @@
+from dotenv import load_dotenv
+import os
+from groq import Groq
+import requests
+from bs4 import BeautifulSoup
+from nltk.tokenize import sent_tokenize
+import nltk
+
+load_dotenv(".env")
+
+client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
+
+nltk.download("punkt_tab")
+def clean_text(text):
+    sentences = sent_tokenize(text)
+    cleaned_text = ' '.join(sentences[:50])
+    return cleaned_text
+
+def summarize_text(input_text):
+    try:
+        res = client.chat.completions.create(
+            messages=[
+                {"role": "system", "content": f"Summarize this text in meaningful terms and no more than 200 words:\n\n{input_text}"},
+            ],
+            temperature=0.3,
+            max_tokens=200,
+            model="llama3-8b-8192",
+        )
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None     
+
+    summary = res.choices[0].message.content
+    return summary
+
+def scrape_then_summarize(url:str):
+    try:
+        response = requests.get(url)
+        if response.status_code != 200:
+            print(f"Failed to get content from {url}")
+            return None
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        print("Scrapping finished. Cleaning...")
+
+        paragraphs = soup.find_all('p')
+        page_text = ' '.join([p.text for p in paragraphs])
+        cleaned_text = clean_text(page_text)
+
+        if not cleaned_text or len(cleaned_text.strip()) < 50:
+            print("Not enough content to summarize.")
+            return None
+        print("Cleaning finished. Summarizing...")
+
+        summary = summarize_text(cleaned_text)
+        if summary:
+            print("Summary generation finished.")
+            return summary
+        else:
+            print("Failed to generate summary.")
+
+    except Exception as e:
+        print(f"An error occurred while scraping or summarizing: {e}")
+        return None
+
