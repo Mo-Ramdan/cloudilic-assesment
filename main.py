90808948767635a821c98e5029e74339719ddc40
Mohamed Ramadan <mohameda7maa@gmail.com>
2024-09-06 23:48:53 +0300

    Adding python runtime

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.environ.get("FRONTEND_URL")],  
+    allow_origins=[os.environ.get("FRONTEND_URL") , "*"],  
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"], 
